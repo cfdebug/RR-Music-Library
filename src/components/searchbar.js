95689a719993eaf3af1141a4ba0865b231dc2259
cfdebug <rfadventure22@gmail.com>
2022-11-12 17:04:02 -0600

    Completed Bonus

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -7,7 +7,7 @@ const SearchBar = (props) => {
     <div>
         <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
             
-            <input type='text' placeholder='Enter a search term here' onChange={(e) => setSearchTerm(e.target.value)} />
+            <input type='text' placeholder='Enter a search term here' onChange={(e) => props.handleSearch(e, e.target.value)} />
             
             <input type='submit' />
         </form>
